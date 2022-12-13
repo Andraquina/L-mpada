c161dbfc6550e9be388df1488d1d3f2c5b382168
Andraquina <115549652+Andraquina@users.noreply.github.com>
2022-12-13 13:31:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function changeImage() {
+  if (
+    document.getElementById("lamp").src ===
+    "https://i.postimg.cc/KjK1wL3c/bulb-off.png"
+  ) {
+    document.getElementById("lamp").src =
+      "https://i.postimg.cc/6QyTynzr/bulb-on.png";
+    document.getElementById("button").innerHTML = "Desligar";
+    document.getElementById("main").classList.remove("lightoff");
+    document.getElementById("main").classList.add("lighton");
+  } else if (
+    document.getElementById("lamp").src ===
+    "https://i.postimg.cc/6QyTynzr/bulb-on.png"
+  ) {
+    document.getElementById("lamp").src =
+      "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
+    document.getElementById("button").innerHTML = "Ligar";
+    document.getElementById("main").classList.add("lightoff");
+    document.getElementById("main").classList.remove("lighton");
+  }
+}
+
+document.getElementById("button").addEventListener("click", changeImage);
